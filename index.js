8e038330ad9b32f590290e16d0161a42b8003cf2
Jobwawesh <jobnjunge15@gmail.com>
2022-12-13 12:06:45 +0300

    Code challenge 3

--- index.js
+++ index.js
@@ -1,171 +1,80 @@
-document.addEventListener('DOMContentLoaded', () => { moviesLoaded() })
-document.addEventListener('DOMContentLoaded', moviesLoaded)
-
-
-  //fetches all movie details upon page load
-  function moviesLoaded(){
-
-    const listMovies = document.getElementById('films')
-
-    const movieContainer = document.getElementById('movieContainer')
-
-    const posterDiv = document.getElementById('posterDiv')
-
-showMovie(1, movieContainer)
-    fetch('https://sammy-ck.github.io/db.json')
+let URL = 'http://localhost:3000/films'
+const listHolder = document.getElementById('films')
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementsByClassName('film item')[0].remove()
+    fetchMovies(URL)
+})
+//Create fetch function to get the data from the db.json
+function fetchMovies(URL){
+    fetch(URL)
     .then(resp => resp.json())
-    .then(data => createMovieDetailCard(data.films[0], movieContainer))
-
-    fetch('http://localhost:3000/films')
-
-    fetch('https://sammy-ck.github.io/db.json')
-    .then(resp => resp.json())
-    .then(data => {
-
-        data.forEach(movie => {
-        (data.films).forEach(movie => {
-            const liMovie = document.createElement('li')
-            liMovie.style.cursor = 'pointer'
-
-            liMovie.addEventListener('click', () => {
-                movieContainer.innerHTML = ''
-               showMovie(movie.id, movieContainer)
-            })
-
-            const pTitle = document.createElement('p')
-            const deleteBtn = document.createElement('button')
-            deleteBtn.innerText = 'DELETE'
-
-            deleteBtn.addEventListener('click',() => { 
-                deleteMovie(movie.id, liMovie)
-            })
-
-            pTitle.innerText = movie.title
-            liMovie.appendChild(pTitle)
-            pTitle.appendChild(deleteBtn)
-            listMovies.appendChild(liMovie)
-
-            liMovie.addEventListener('click', () => {
-                movieContainer.innerHTML = ''
-                createMovieDetailCard(movie, movieContainer)
-            })
-        })
+    .then(movies => {
+        movies.forEach(movie => {
+            displayMovie(movie)
+        });
     })
-  }
 }
-
-//createMovieDetailsCard : Creates a movie detail card from data using fetch request response for individual movie
-/**
-* createMovieDetailsCard : Creates a movie detail card for movie.
-*   
-*   @data: fetch request response for individual movie
-**/
-function createMovieDetailCard(data, tagToAppend) {
-
-    const poster = document.createElement('img')
-        poster.src = data.poster
-
-        tagToAppend.appendChild(poster)
-        const title = document.createElement('h2')
-        title.innerText = data.title
-        tagToAppend.appendChild(title)
-        const runtime = document.createElement( 'p')
-        runtime.innerHTML = `<b>Run time:</b> ${data.runtime}`
-        tagToAppend.appendChild(runtime)
-        const showtime = document.createElement( 'p')
-        showtime.innerHTML = `<b>Show time:</b> ${data.showtime}
-        <p><b>Available Tickets</b></p>`
-        tagToAppend.appendChild(showtime)
-
-////Calculate available movie tickets
-        const capacity = data.capacity
-        const titcketsSold = data.tickets_sold
-        let remainingTickets = capacity - titcketsSold
-
-        const availableTickets = document.createElement('p')
-        availableTickets.innerText =  remainingTickets
-        tagToAppend.appendChild(availableTickets)
-        const description = document.createElement( 'p')
-        description.innerText = data.description
-        tagToAppend.appendChild(description)
-
-        const buyTicketBtn = document.createElement('button')
-        buyTicketBtn.innerText = 'BUY TICKET'
-
-
-updateBuyBtn (remainingTickets, availableTickets, buyTicketBtn)
-
-//update DOM and json server on available tickets upon buying
-        buyTicketBtn.addEventListener('click',() => {
-            fetch(`http://localhost:3000/films/${data.id}`)
-            .then(resp => resp.json())
-            .then(datam => {
-
-            fetch(`http://localhost:3000/films/${data.id}`,{
-            method : "PATCH",
-            headers : {
-            "Content-Type" : "application/json",
-            Accept : "application/json"
-            },
-            body : JSON.stringify({
-            tickets_sold : ((datam.tickets_sold < datam.capacity)?datam.tickets_sold + 1: datam.capacity)
+//function to display the titles of the movies as a list
+function displayMovie(movie){
+    const list = document.createElement('li')
+    list.style.cursor="pointer"
+    list.textContent= (movie.title)
+    listHolder.appendChild(list)
+    addClickEvent()
+}
+//Adding the click event listener
+function addClickEvent(){
+    let children=listHolder.children
+    for(let i=0; i<children.length; i++){
+        let child=children[i]
+        // console.log(child) <= to check if have the right child
+        child.addEventListener('click',() => {
+            fetch(`${URL}/${i+1}`)
+            .then(res => res.json())
+            .then(movie => {
+                document.getElementById('buy-ticket').textContent = 'Buy Ticket'
+                setUpMovieDetails(movie)
             })
-            })
-            .then(resp => resp.json())
-            .then(dataz => {
-            remainingTickets--
-
-            updateBuyBtn (remainingTickets, availableTickets, buyTicketBtn)
-            if(remainingTickets > 1){
-                remainingTickets--
-               availableTickets.innerText =  remainingTickets
-            }else{
-                availableTickets.innerText = 0
-                buyTicketBtn.disabled = true
-                buyTicketBtn.innerText = 'SOLD OUT'
-                buyTicketBtn.style.cursor = 'not-allowed'
-                buyTicketBtn.style.backgroundColor = 'grey'
-            }
-
-
-            })
-            })
-
         })
-        tagToAppend.appendChild(buyTicketBtn)
-}
-
-
-//shows movie details on the DOM
-function showMovie(id, movieContainer) {
-    fetch(`http://localhost:3000/films/${id}`)
-    .then(resp => resp.json())
-    .then(data => createMovieDetailCard(data, movieContainer))
     }
-
-
-//deletes a movie from the ul menu list and the server
-function deleteMovie (movieID, liMovie)  {
-    fetch(`http://localhost:3000/films/${movieID}`,{
-      method : "DELETE",
-      headers : {
-          "Content-Type" : "application/json",
-          Accept : "application/json"
-      }
-    })
-    .then(() => { liMovie.remove(); })
-  }
-
-//disable button on sold out tickets
-function updateBuyBtn (remainingTickets, availableTickets, buyTicketBtn){
-    if(remainingTickets >= 1){                
-        availableTickets.innerText =  remainingTickets
-     }else{
-         availableTickets.innerText = 0
-         buyTicketBtn.disabled = true
-         buyTicketBtn.innerText = 'SOLD OUT'
-         buyTicketBtn.style.backgroundColor = 'grey'
-         buyTicketBtn.style.cursor = 'not-allowed'
-     }
-  }
 }
+//Posting movie details
+// poster to be dispalyed on the div with poster id
+
+function setUpMovieDetails(funMovie){
+    const preview = document.getElementById('poster')
+    preview.src = funMovie.poster;
+//title
+
+    const movieTitle = document.querySelector('#title');
+    movieTitle.textContent = funMovie.title;
+    //runtime
+
+    const movieTime = document.querySelector('#runtime');
+    movieTime.textContent = `${funMovie.runtime} minutes`;
+    //description
+
+    const movieDescription = document.querySelector('#film-info');
+    movieDescription.textContent = funMovie.description;
+    //Showtime
+
+    const showTime = document.querySelector('#showtime')
+    showTime.textContent = funMovie.showtime;
+    // available tickets =capacity - tickets sold
+
+    const tickets  = document.querySelector('#ticket-number')
+    tickets.textContent = funMovie.capacity -funMovie.tickets_sold;
+}
+// //Sold out
+
+const btn = document.getElementById('buy-ticket')
+btn.addEventListener('click', function(event){
+    let remainingTickets = document.querySelector('#ticket-number').textContent
+    event.preventDefault()
+    if(remainingTickets > 0){
+        document.querySelector('#ticket-number').textContent  = remainingTickets-1
+    }
+    else if(parseInt(remTickets, 10)===0){
+    btn.textContent = 'Sold Out'
+    }
+})
